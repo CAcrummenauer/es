9b586e49697a79db1b4d9730bf00181e0c2c93b8
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-07 04:09:29 -0200

    Consumindo uma api do Github usando requisições AJAX...

--- index.js
+++ index.js
@@ -1,47 +1,10 @@
-var listElement = document.querySelector('#app ul');
-var inputElement = document.querySelector('#app input');
-var buttonElement = document.querySelector('#app button');
-inputElement.on
+var xhr = new XMLHttpRequest();
 
-var tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+xhr.open('GET', 'http://api.github.com/users/cacrummenauer');
+xhr.send(null);
 
-function adicionarTarefas() {
-    listElement.innerHTML = '';
-    for (tarefa of tarefas) {
-        var liElement = document.createElement('li');
-        var liTextElement = document.createTextNode(tarefa + ' ');
-
-        var linkElement = document.createElement('a');
-        linkElement.setAttribute('href', '#');
-        // Obter índice correspondente ao texto da tarefa
-        var posicao = tarefas.indexOf(tarefa);
-        linkElement.setAttribute('onclick', 'removerTarefa(' + posicao + ')');
-        var linkTextElement = document.createTextNode('Remover');
-        linkElement.appendChild(linkTextElement);
-
-        liElement.appendChild(liTextElement);
-        liElement.appendChild(linkElement);
-        listElement.appendChild(liElement);
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+        console.log(JSON.parse(xhr.responseText));
     }
-}
-adicionarTarefas();
-
-function adicionarTarefa() {
-    var liTextElement = inputElement.value;
-    tarefas.push(liTextElement);
-    inputElement.value = '';
-    adicionarTarefas();
-    saveToStorage();
-}
-
-buttonElement.onclick = adicionarTarefa;
-
-function removerTarefa(posicao) {
-    tarefas.splice(posicao, 1);
-    adicionarTarefas();
-    saveToStorage();
-}
-
-function saveToStorage() {
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
 }
